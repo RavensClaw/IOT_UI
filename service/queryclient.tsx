6d5a1ea6fae00cd3bd9d9f63b4e9adbe5583a093
RavensClaw <jiten1983@gmail.com>
2025-09-02 09:03:52 +0530

    fixed versions and web

--- service/queryclient.tsx
+++ service/queryclient.tsx
@@ -8,7 +8,7 @@ const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       retry: 2,
-      staleTime: 1000 * 60 * 5, // 5 mins
+      //staleTime: 1000 * 60 * 5, // 5 mins
     },
   },
 });
